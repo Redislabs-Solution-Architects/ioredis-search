5cbc7f18d6236a613e27e3578d7cec2f2525d440
joeywhelan <joey.whelan@redis.com>
2022-09-02 14:10:43 -0600

    add - comments to main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const NUM = 10;
 const COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 async function load(client) {
+    //Create pipeline to build 10 hash sets
     const pipeline = client.pipeline();
-
     for (let i=0; i < NUM; i++) {
         const colors = (COLORS.sort(() => .5 - Math.random())).slice(0, Math.floor(Math.random() * COLORS.length))
         const fields = {
@@ -22,36 +22,46 @@ async function load(client) {
     }
     await pipeline.exec();
 
+    //Build index for those hashes
     await client.call('FT.CREATE', 'idx', 'ON', 'HASH', 'PREFIX', '1', 'item:', 'SCHEMA', 
         'textField', 'TEXT', 'SORTABLE',
         'numericField', 'NUMERIC', 'SORTABLE',
         'tagField', 'TAG'
     );
+
+    //Print the INFO for the index
     const result = await client.call('FT.INFO', 'idx');
     console.log(`FT.INFO idx - ${JSON.stringify(result)}\n`);
 }
 
 async function search(client) {
+    //Search for exact match in a Text field
     let result = await client.call('FT.SEARCH', 'idx', '@textField:text1');
     console.log(`FT.SEARCH idx @textField:text1 - ${JSON.stringify(result)}\n`);
 
+    //Search for a range in a Numeric field
     result = await client.call('FT.SEARCH', 'idx', '@numericField:[1,3]');
     console.log(`FT.SEARCH idx @numericField:[1,3] - ${JSON.stringify(result)}\n`);
 
+    //Search for a match in a Tag Field
     result = await client.call('FT.SEARCH', 'idx', '@tagField:{blue}');
     console.log(`FT.SEARCH idx @tagField:{blue} - ${JSON.stringify(result)}\n`);
 }
 
 async function aggregate(client) {
+    //Aggregate on a text field across all hashes in the index
     let result = await client.call('FT.AGGREGATE', 'idx', '*', 'GROUPBY', '1', '@textField', 
         'REDUCE', 'COUNT', '0', 'AS', 'CNT');
     console.log(`FT.AGGREGATE idx * GROUPBY 1 @textField REDUCE COUNT 0 AS CNT - ${JSON.stringify(result)}\n`);
 
+    //Search on a numeric range and then apply the SQRT function to a numeric field in the matches
     const upper = Math.floor(Math.random() * NUM) + 1;
     result = await client.call('FT.AGGREGATE', 'idx', `@numericField:[0,${upper}]`, 
         'APPLY', 'SQRT(@numericField)', 'AS', 'SQRT');
     console.log(`FT.AGGREGATE idx @numericField:[0,${upper}] APPLY SQRT(@numericField) AS SQRT - ${JSON.stringify(result)}\n`);
 
+    //Search for logical OR of two tag values and use a CURSOR.  Limit return
+    //to 2 values to illustrate looping on a cursor.
     result = await client.call('FT.AGGREGATE', 'idx', '@tagField:{ yellow | red }', 
         'LOAD', '3', '@textField', '@numericField', '@tagField',
         'WITHCURSOR', 'COUNT', '2'
@@ -80,16 +90,19 @@ async function aggregate(client) {
 async function alter(client) {
     console.log('Adding additional text field "newField" to all hashes and altering index');
     
+    //Set up a pipeline to add a new field to all the existing hashes.
     const pipeline = client.pipeline();
     for (let i=0; i < NUM; i++) {
         await pipeline.hset(`item:${i}`, 'newField', `new${Math.floor(Math.random() * NUM)}`)
     }
     await pipeline.exec();
 
+    //Alter the existing index by adding this new field
     await client.call('FT.ALTER', 'idx', 'SCHEMA', 'ADD', 'newField', 'TEXT', 'SORTABLE');
     let result = await client.call('FT.INFO', 'idx');
     console.log(`FT.INFO idx - ${JSON.stringify(result)}\n`);
 
+    //Search for this new field in the index
     result = await client.call('FT.SEARCH', 'idx', '@newField:new1');
     console.log(`FT.SEARCH idx @newField:new1 - ${JSON.stringify(result)}\n`);
 }
@@ -110,4 +123,3 @@ async function alter(client) {
         await client.quit();
     }
 })();
-
